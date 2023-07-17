a2ec7f8a00ff6224f229285ee475cfba8d56d20c
Timur <130249301+905timur@users.noreply.github.com>
2023-07-17 11:12:08 -0300

    Update printerLookup.py

--- printerLookup.py
+++ printerLookup.py
@@ -2,13 +2,12 @@ import socket
 import threading
 from queue import Queue
 
-target_subnet = '192.168.1.0/24' 
-
-printers = []
+def ip_range(target_subnet):
+    return [ip for ip in range(target_subnet.split("/")[0], target_subnet.split("/")[0] + 256)]
 
 def portscan(ip, port):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((ip, port))
         return True
     except:
@@ -29,14 +28,23 @@ def scan(ip):
     if portscan(ip, 9100):
         get_printer_info(ip, 9100)
 
-for ip in ip_range(target_subnet):
-    queue.put(ip)
+try:
+    printers = []
+    queue = Queue()
+    for ip in ip_range(target_subnet):
+        queue.put(ip)
 
-for i in range(256):
-    t = threading.Thread(target=scan, args=(queue.get(),))
-    t.daemon = True
-    t.start()
+    for i in range(256):
+        t = threading.Thread(target=scan, args=(queue.get(),))
+        t.daemon = True
+        t.start()
 
-with open('printers.txt', 'w') as f:
-    for printer in printers:
-        f.write(f"{printer['ip']} {printer['brand']} {printer['model']}\n")
+    for t in threading.enumerate():
+        if not t.daemon:
+            t.join()
+
+    with open('printers.txt', 'w') as f:
+        for printer in printers:
+            f.write(f"{printer['ip']} {printer['brand']} {printer['model']}\n")
+except Exception as e:
+    print(e)
